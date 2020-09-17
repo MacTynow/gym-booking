bcbfd9eeac01f6307470298a3cba67ddcce27813
Charles Martinot <charles.martinot@grab.com>
2020-09-17 15:39:05 +0800

    Fix sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/bin/python3 /Users/charles.martinot/mactynow/fuckevolve/fuck-evolve.py >> /Users/charles.martinot/mactynow/fuckevolve/fuck-evolve.log
+/usr/local/bin/python3 main.py
